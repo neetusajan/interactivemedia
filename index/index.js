1be6f186281f4fd3e9b41e1cf5518214243fa643
neetusajan <neetu.sajan@gmail.com>
2021-06-28 21:22:02 -0400

    EDITSS

--- index/index.js
+++ index/index.js
@@ -1,1 +1,17 @@
+jQuery(document).ready(function() {
 
+  var mouseX = 0, mouseY = 0;
+  var xp = 0, yp = 0;
+   
+  $(document).mousemove(function(e){
+    mouseX = e.pageX - 30;
+    mouseY = e.pageY - 30; 
+  });
+    
+  setInterval(function(){
+    xp += ((mouseX - xp)/2);
+    yp += ((mouseY - yp)/2);
+    $("#circle").css({left: xp +'px', top: yp +'px'});
+  }, 20);
+
+});
